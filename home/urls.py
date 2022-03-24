531d2956e649d3a18a721ea6de057df495787dff
zohaib <89574767+zohaib000@users.noreply.github.com>
2022-03-24 07:23:03 -0700

    Update urls.py

--- home/urls.py
+++ home/urls.py
@@ -3,11 +3,5 @@ from django.urls import path,include
 from home import views
 
 urlpatterns = [
-  path("",views.story,name="story"),
-  path("story_submit",views.story_submit,name="story_submit"),
-  path("post",views.post,name="post"),
-  path("post_submit",views.post_submit,name="post_submit"),
-  path("profile",views.profile,name="profile"),
-  path("profile_submit",views.profile_submit,name="profile_submit"),
-
-]
+      path("",views.home.as_view(),name="home"),
+  ]
